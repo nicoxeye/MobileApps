d29efb9494fc6f2832f7be1da49349e5cdc38191
nick✮⋆˙ <162123881+nicoxeye@users.noreply.github.com>
2025-10-28 16:42:57 +0100

    part 1 have to check it :]

---
+++ Lab3Homework/app/src/main/java/com/example/lab3homework/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.lab3homework;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        EditText typePlace = findViewById(R.id.editTextText);
+        Button send = findViewById(R.id.button);
+
+        send.setOnClickListener(v -> sendToPlace(typePlace));
+    }
+
+    void sendToPlace(EditText typePlace) {
+        String place = typePlace.toString().trim();
+        Uri mapIntentUri = Uri.parse("google.navigation:q=" + Uri.encode(place));
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, mapIntentUri);
+    }
+}
